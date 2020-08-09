174df9fab6878e739a7a8340b6a56de836a20da2
Kit Fuderich <kitfuderich@gmail.com>
2020-08-09 16:28:52 -0700

    changed styling into cards for the chat component and adjusted field for inputting comment to not require a name

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {CardHeader, Card, Button, Modal, ModalHeader, ModalBody, Label, Row, Col, CardBody } from 'reactstrap';
+import {CardHeader, Card, Button, Modal, ModalHeader, ModalBody, Label, Row, Col, CardBody, CardText } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import Timer from './TimerComponent';
 
@@ -18,21 +18,27 @@ function RenderComments({comments,deleteComment, upComment,downComment}){
     <CardBody>
         <div className="container">
 <div className="row">
-    <div className="col-4">
+    <div className="col-12 col-md-4">
+        <Card>
+           
         <CardHeader>
         Up: {info.thumbsUp}
-        {info.startTimer? <span style={buttonStyle} onClick={()=>{upComment(info)}} className="fa fa-thumbs-up"></span>: null}
+        {info.startTimer? <span style={buttonStyle} onClick={()=>{upComment(info)}} className="fa fa-thumbs-up fa-4x"></span>: null}
         
         </CardHeader>
     <CardHeader>
     Down:{info.thumbsDown}
-    {info.startTimer?  <span style={buttonStyle} onClick={()=>{downComment(info)}} className="fa fa-thumbs-down"></span>: null}
+    {info.startTimer?  <span style={buttonStyle} onClick={()=>{downComment(info)}} className="fa fa-thumbs-down fa-4x"></span>: null}
    
     </CardHeader>
+        </Card>
+   
    
     </div>
-    <div className="col-7">
-    <ol>
+    <div className="col-12 col-md-7">
+    
+              
+                <ol>
      <p style = {textStyle}>{info.comment}</p>
   
      <p>-- {info.author.firstname}<span> </span>
@@ -46,6 +52,9 @@ function RenderComments({comments,deleteComment, upComment,downComment}){
 
      </p>
      </ol> 
+               
+     
+   
     </div>
 
 
@@ -90,6 +99,7 @@ class Chat extends Component {
         this.handlePost = this.handlePost.bind(this);
         this.checkLogin = this.checkLogin.bind(this);
         this.renderTimer = this.renderTimer.bind(this);
+        this.endVoting = this.endVoting.bind(this);
             }
 
     
@@ -194,12 +204,19 @@ renderTimer(comments,toggleTimer, timerActive, minutes,seconds){
         });
       }
 
+      endVoting(){
+          this.setState({
+              minutes:0,
+              seconds:0
+          })
+      }
+
       handlePost(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        this.props.postComment(values.comment, values.author)
+       
+        this.props.postComment(values.comment, this.props.user)
         
         this.setState({
-            seconds:11,
+            seconds:50,
             timerActive:true
         })
         localStorage.setItem('timerActive',this.state.timerActive)
@@ -228,29 +245,10 @@ const { minutes, seconds } = this.state
 <div className="container">
 
              <Row className="form-group">
-                                <Label htmlFor="firstname" md={12}>Your Name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".author" id="author" name="author"
-                                        placeholder="Your Name"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
-                                         />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".firstname"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                     />
-                                </Col>
+                              
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Label htmlFor="comment" md={12}>Comment:</Label>
                                 <Col md={10}>
                                     <Control.textarea model=".comment" id="comment"
                                         rows="6"
@@ -273,21 +271,32 @@ const { minutes, seconds } = this.state
 <div className ="row">
 
 <div className="col-6">
-<div>User:</div>
-{this.props.user}
+
+{this.props.user} <br/>
 
 <Button outline onClick={this.checkLogin} color="primary">Submit Comment</Button>
 <div>
                 { minutes === 0 && seconds === 0
-                    ? <h1>Voting Complete.</h1>
-                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                    ? <Card>
+                        <CardHeader>Voting Complete.</CardHeader>
+                    </Card>
+                    : 
+                    <Card>
+                        <CardHeader>
+                        Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                        </CardHeader>
+                        <CardBody> <Button onClick={this.endVoting}>End Voting</Button></CardBody>
+                    </Card>
+                   
+                    
+                  
                 }
             </div>
 
 </div>
 
 <div className = "col-6">
-<div>Comments:</div>
+<div>Votes:</div>
 <RenderComments comments = {this.props.comments} deleteComment = {this.props.deleteComment} auth={this.props.auth} upComment = {this.props.upComment} downComment={this.props.downComment} toggleTimer={this.props.toggleTimer}/>
 
 </div>
